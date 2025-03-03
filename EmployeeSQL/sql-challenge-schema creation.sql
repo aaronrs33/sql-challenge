6e02b2e558edb069b4dee1d7e6adf7a42f278ee6
aaronrs33 <aaronrs33@gmail.com>
2025-03-02 21:57:44 -0800

    Update sql-challenge-schema creation.sql

--- EmployeeSQL/sql-challenge-schema creation.sql
+++ EmployeeSQL/sql-challenge-schema creation.sql
@@ -34,7 +34,7 @@ CREATE TABLE dept_emp (
 	emp_no INT PRIMARY KEY,
 	dept_no VARCHAR(10),
 	PRIMARY KEY (emp_no, dept_no),
-	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
 	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
 );
 
