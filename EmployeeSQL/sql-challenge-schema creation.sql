1c57af44febd2f2bdce0c185a0eba76bd60ed6af
aaronrs33 <aaronrs33@gmail.com>
2025-03-02 21:53:26 -0800

    Added .sql file of how I created Schema's. Did not realize two separate files were needed.

---
+++ EmployeeSQL/sql-challenge-schema creation.sql
@@ -0,0 +1,48 @@
+-- Create the departments table
+CREATE TABLE departments (
+	dept_no VARCHAR(10) PRIMARY KEY,
+	dept_name VARCHAR(255) NOT NULL UNIQUE
+);
+
+--Create the titles table
+CREATE TABLE titles (
+	title_id VARCHAR(10) PRIMARY KEY,
+	title VARCHAR(255) NOT NULL
+);
+
+-- Create the employees table
+CREATE TABLE employees (
+	emp_no INT PRIMARY KEY,
+	emp_title_id VARCHAR(10),
+	birth_date DATE NOT NULL,
+	first_name VARCHAR(50) NOT NULL,
+	last_name VARCHAR(50) NOT NULL,
+	sex CHAR(1) CHECK (sex IN ('M','F')),
+	hire_date DATE NOT NULL,
+	FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
+);
+
+-- Create the salaries table
+CREATE TABLE salaries (
+	emp_no INT PRIMARY KEY,
+	salary INT NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
+
+--Create the dept_emp table
+CREATE TABLE dept_emp (
+	emp_no INT PRIMARY KEY,
+	dept_no VARCHAR(10),
+	PRIMARY KEY (emp_no, dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+);
+
+-- Create the dept_manager table
+CREATE TABLE dept_manager (
+	dept_no VARCHAR(10),
+	emp_no INT,
+	PRIMARY KEY (dept_no, emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
